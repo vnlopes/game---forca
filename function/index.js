41bc3e65bfb4ba39f1887b235d537f1f7c9dd419
VINICIUS LOPES DA SILVA <0200832311013>
2024-05-17 12:11:21 -0300

    dadawd

---
+++ function/index.js
@@ -0,0 +1,14 @@
+const word = document.querySelector("#receiveWord");
+
+const init = () => {
+    if (word.value == "") {
+        alert("Digite a palavra!");
+    } else {
+        location.href = "game.html"
+      array.push(word.value);
+  }
+};
+
+let array = [""];
+
+console.log(array);
